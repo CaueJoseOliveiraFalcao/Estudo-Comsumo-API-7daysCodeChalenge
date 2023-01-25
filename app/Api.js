fc01e1aee708c949016d029f047e66220647f686
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-01-25 14:55:20 -0500

    seach

--- app/Api.js
+++ app/Api.js
@@ -1,9 +1,29 @@
+const inputSearch = document.querySelector('.input_procurar')
+const key = '?api_key=2fa38ec709fb23b7b35bacc88a82e18a';
 
-async function getMovies(){
-    const url = 'https://api.themoviedb.org/3/movie/76341?api_key=2fa38ec709fb23b7b35bacc88a82e18a&language=pt-BR'
-    const fetchResponse = await fetch(url)
-    const movies = await fetchResponse.json()
-    console.log(movies)
+function getInput(){
+    InputValue = inputSearch.value
+    var InputValueWhioutSpace = InputValue.replace(/\s/g, '');
+    getMoviesFromInput(InputValueWhioutSpace)
+
 }
 
-getMovies()
+async function getMoviesFromInput(Input){
+
+    const url = `https://api.themoviedb.org/3/search/movie${key}&query=${Input}&language=pt-BR`
+
+    const fetchResponse = await fetch(url)
+    const movies = await fetchResponse.json()
+    let popularMovies = movies.results
+    RenderizarFilmes(popularMovies)
+}
+
+async function ShowPopularMovies(){
+
+    const url = `https://api.themoviedb.org/3/movie/popular${key}&language=en-US&page=1`
+    const response = await fetch(url)
+    const jsonResponse = await response.json()
+    RenderizarFilmes(jsonResponse.results)
+}
+
+ShowPopularMovies()
---
+++ app/PreventDefault.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('.form_procurar')
+document.querySelector(".form_procurar").addEventListener("click", function(event){
+    event.preventDefault()
+  });
